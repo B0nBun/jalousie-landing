73cf4ad2682eb8a4be197c90697692c1dc5a2a2d
r.gusyakov <r.gusyakov@yadro.com>
2026-02-15 16:47:48 +0300

    feat: removed hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,16 +3,27 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
-			black: '#212121',
+			black: '#2F2F2F',
 			white: '#F0EEE9',
-			primary: '#f49114'
+			primary: '#f49114',
 		},
 		extend: {
 			animation: {
+				"card-appear": "card-appear 500ms ease-in-out forwards",
 				"carousell-in": 'carousell-in 500ms ease-in-out forwards',
 				"carousell-out": 'carousell-out 500ms ease-in-out forwards',
 			},
 			keyframes: {
+				"card-appear": {
+					'0%': {
+						transform: 'var(--card-start-transform)',
+						opacity: 0
+					},
+					'100%': {
+						transform: 'translate(0, 0)',
+						opacity: 1
+					}
+				},
 				"carousell-in": {
 					'0%': {
 						transform: 'translateX(var(--carousell-translate))',
